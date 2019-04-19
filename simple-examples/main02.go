7b72c5809cf2e0d3da2ae5bd76005a7b29706812
0xvext <0xvext+github@gmail.com>
2019-04-18 21:45:47 -0400

    New file

---
+++ simple-examples/main02.go
@@ -0,0 +1,14 @@
+/* Write a program which prompts the user to enter a floating point number and
+prints the integer which is a truncated version of the floating point number
+that was entered. Truncation is the process of removing the digits to the
+right of the decimal place. */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, world.")
+}
