97fddeb3c1d26e42ca3fe232d64ac989ca75d0f6
ipatchcables <cscrivana@outlook.com>
2019-04-19 09:33:20 -0400

    a

--- shell/main.go
+++ shell/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:4444") //Send Dial connection to specified ip and port
+	conn, err := net.Dial("tcp", "127.0.0.1:4444") //Send Dial connection to specified ip and port 	 use netcat to test - it works
 	log.Fatal(conn, err)                           // log the conn or err to console
-
 }
