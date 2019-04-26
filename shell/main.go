646f9f09ed80ab3f8dec75d8603c5402d6d4c0ab
ipatchcables <cscrivana@outlook.com>
2019-04-25 21:34:29 -0400

    link to shell code

--- shell/main.go
+++ shell/main.go
@@ -8,4 +8,6 @@ import (
 func main() {
 	conn, err := net.Dial("tcp", "127.0.0.1:4444") //Send Dial connection to specified ip and port 	 use netcat to test - it works
 	log.Fatal(conn, err)                           // log the conn or err to console
+	conn.Close()                                   // Close the connection
+
 }
